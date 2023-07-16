133b064931180c7d730ee443860d249c38a46921
Bux26 <bucs26@live.co.za>
2023-07-16 14:21:00 -0700

    chnages

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
-* excute function
+* main - use puts function to print text
+*
+* Return: Always 0 sucess
 */
 int main(void)
 {
-puts(“Programming is like building a multilingual puzzle \n”;
+puts(“Programming is like building a multilingual puzzle\n”;
 return(0);
 }
-
